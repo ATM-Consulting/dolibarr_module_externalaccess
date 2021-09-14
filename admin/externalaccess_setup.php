e52d03293d73f3306b49599fa219f7ccbd2018e6
Quentin VIAL-GOUTEYRON <quentin.vial-gouteyron@atm-consulting.fr>
2021-09-14 16:32:32 +0200

    FIX too much modification

--- admin/externalaccess_setup.php
+++ admin/externalaccess_setup.php
@@ -199,11 +199,11 @@ print_multiselect('EACCESS_LIST_ADDED_COLUMNS_SHIPPING', false, $TAddedColumnShi
 print_multiselect('EACCESS_LIST_ADDED_COLUMNS_PROJECT', false, array('budget_amount'=>$langs->trans('Budgets')));
 $e = new ExtraFields($db);
 $e->fetch_name_optionals_label('commande');
-$TExtrafields_propal = array_keys($e->attributes['commande']['list']);
-foreach ($TExtrafields_propal as $ef_name) {
-    $TExtrafields_propal_list['EXTRAFIELD_'.$ef_name] = $e->attributes['commande']['label'][$ef_name];
+$TExtrafields_commande = array_keys($e->attributes['commande']['list']);
+foreach ($TExtrafields_commande as $ef_name) {
+    $TExtrafields_commande_list['EXTRAFIELD_'.$ef_name] = $e->attributes['commande']['label'][$ef_name];
 }
-print_multiselect('EACCESS_LIST_ADDED_COLUMNS_ORDER', false, $TExtrafields_propal_list);
+print_multiselect('EACCESS_LIST_ADDED_COLUMNS_ORDER', false, $TExtrafields_commande_list);
 
 $e->fetch_name_optionals_label('propal');
 $TExtrafields_propal = array_keys($e->attributes['propal']['list']);
