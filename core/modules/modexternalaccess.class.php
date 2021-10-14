65bfd81e966c6f18fbac74bf2a8b862292696073
atm-lena <lena.papazian@atm-consulting.fr>
2021-10-14 15:08:03 +0200

    NEW : function print_propalTable : parameter link or not to propal

--- core/modules/modexternalaccess.class.php
+++ core/modules/modexternalaccess.class.php
@@ -64,7 +64,7 @@ class modexternalaccess extends DolibarrModules
 
 
 
-		$this->version = '1.25.0';
+		$this->version = '1.26.0';
 
 
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
