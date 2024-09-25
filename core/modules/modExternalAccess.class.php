da8fa715bbc13034dd2a75ab466d48a3d4c67c9e
ATM-nicolasV <nicolas.vidal@atm-consutling.fr>
2024-09-25 15:55:53 +0200

    recupération du dev de irvine et ajout de trad

--- core/modules/modExternalAccess.class.php
+++ core/modules/modExternalAccess.class.php
@@ -59,7 +59,7 @@ class modExternalAccess extends DolibarrModules
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i', '', get_class($this));
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
-		$this->description = "Ajoute un acces externe pour les clients";
+		$this->description = 'externalaccessDesc' ;
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
 
 		$this->version = '1.44.1';
