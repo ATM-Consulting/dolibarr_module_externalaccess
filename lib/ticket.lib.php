c4a423a9e95151acc11ea5709f6c6c5af78c15eb
atm-florian <florian.mortgat@atm-consulting.fr>
2025-08-07 11:53:34 +0200

    FIX warnings when getDolGlobalString('EACCESS_CARD_ADDED_FIELD_TICKET') === ''

--- lib/ticket.lib.php
+++ lib/ticket.lib.php
@@ -672,7 +672,7 @@ function print_ticketCard_extrafields($ticket) {
 	$out = '';
 	$e = new ExtraFields($db);
 	$e->fetch_name_optionals_label('ticket');
-	$TTicketAddedField = explode(',', getDolGlobalString('EACCESS_CARD_ADDED_FIELD_TICKET'));
+	$TTicketAddedField = getDolGlobalString('EACCESS_CARD_ADDED_FIELD_TICKET') ? explode(',', getDolGlobalString('EACCESS_CARD_ADDED_FIELD_TICKET')) : [];
 	if(! empty($TTicketAddedField)) {
 		foreach($TTicketAddedField as $ticket_field) {
 			$ticket_field = strtr($ticket_field, array('EXTRAFIELD_' => ''));
